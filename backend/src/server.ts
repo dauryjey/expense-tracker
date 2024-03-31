9b8d473a030c985db7eb5f872517bc6e940a8011
Daury P. <dauryjey@gmail.com>
2024-03-31 18:11:25 -0400

    Added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express"
+import cors from "cors"
 import "dotenv/config"
 import Paths from "./const/Paths"
 import apiRouter from "./routes/api"
 
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
